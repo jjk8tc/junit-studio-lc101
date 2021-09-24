507819fdbe5586e8dde7e8e63b3207ab9b9c7d79
Kroese <Josh.Kroese@fike.com>
2021-09-24 09:07:23 -0500

    completed class 5 studio on unit testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -12,5 +12,58 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void noBracketTest(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("abcd"));
+    }
+
+    @Test
+    public void oneOpenBracketTest(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("[abcd"));
+    }
+
+    @Test
+    public void oneCloseBracketTest(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("abcd]"));
+    }
+
+    @Test
+    public void outOfOrderBracketTest(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("]abcd["));
+    }
+
+    @Test
+    public void nestedBracketsTest(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[one[two[three]]]"));
+    }
+
+    @Test
+    public void failNestedBracketsTest(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("[one[two[three]]"));
+    }
+
+    @Test
+    public void failNestedBrackets2Test(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("[one[two[three]]]]"));
+    }
+
+    @Test
+    public void failNestedBrackets3Test(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("[[[[one[two]three]]"));
+    }
+
+    @Test
+    public void curlyBracketsTest(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[one[{two]]"));
+    }
+
+    @Test
+    public void curlyBrackets2Test(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("[one{two]]"));
+    }
 }
